287eea70fb47b351f712f9d1246d825e1c83834b
unknown <zeronovzeron950@gmail.com>
2025-04-23 20:58:36 +0600

    shop

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import { useState } from 'react'
-import { Header } from './components/Header/Header'
-import { About } from './components/About/About'
-import { Slaider } from './components/Slaider/Slaider'
-import { Shop } from './components/Shoop/Shop'
-import { Aboutdog } from './components/Aboutdog/Aboutdog'
-import { Footer } from './components/Foooter/Footer'
-
+import { useState } from "react";
+import { Header } from "./components/Header/Header";
+import { About } from "./components/About/About";
+import { Slaider } from "./components/Slaider/Slaider";
+import { Shop } from "./components/Shoop/Shop";
+import { Aboutdog } from "./components/Aboutdog/Aboutdog";
+import { Footer } from "./components/Foooter/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <Header />
-      <About /> 
-     <Slaider />
-     <Shop />
-     <Aboutdog />
-     <Footer />
+      <Header />
+      <About />
+      <Slaider />
+      <Shop />
+      <Aboutdog />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Shoop/Shop.jsx
+++ src/components/Shoop/Shop.jsx
@@ -1,79 +1,82 @@
 import React from "react";
 import shopProduct from "./Shop.module.css";
-import foods from "../../../assets/icons/foods.svg";
-import transport from "../../../assets/icons/foods.svg";
-import toys from "../../../assets/icons/foods.svg";
-import bowls from "../../../assets/icons/foods.svg";
-import shampoos from "../../../assets/icons/foods.svg";
-import vitamins from "../../../assets/icons/foods.svg";
-import medicines from "../../../assets/icons/foods.svg";
-import leashes from "../../../assets/icons/foods.svg";
-import sleeping from "../../../assets/icons/foods.svg";
+import foods from "../../assets/icons/foods.svg";
+import transport from "../../assets/icons/transport.svg";
+import toys from "../../assets/icons/toys.svg";
+import bowls from "../../assets/icons/bowls.svg";
+import shampoos from "../../assets/icons/shampoos.svg";
+import vitamins from "../../assets/icons/vitamins.svg";
+import medicines from "../../assets/icons/medicines.svg";
+import leashes from "../../assets/icons/leashes.svg";
+import sleeping from "../../assets/icons/sleeping-area.svg";
 
 export const Shop = () => {
   const shopIcon = [
     {
       id: 1,
       icon: foods,
-      title: Petfood,
+      title: "Pet food",
     },
     {
       id: 2,
       icon: transport,
-      title: Transportation,
+      title: "Transportation",
     },
     {
       id: 3,
       icon: toys,
-      title: Toys,
+      title: "Toys",
     },
     {
       id: 4,
-      icon: bawls,
-      title: Bowlsandcups,
+      icon: bowls,
+      title: "Bowls and cups",
     },
     {
       id: 5,
       icon: shampoos,
-      title: Shampoos,
+      title: "Shampoos",
     },
     {
       id: 6,
       icon: vitamins,
-      title: Vitamins,
+      title: "Vitamins",
     },
     {
       id: 7,
       icon: medicines,
-      title: Medicines,
+      title: "Medicines",
     },
     {
       id: 8,
       icon: leashes,
-      title: Collars / leashes,
+      title: "Collars / leashes",
     },
     {
       id: 9,
       icon: sleeping,
-      title: Sleepingareas,
+      title: "Sleeping areas",
     },
   ];
 
   return (
     <>
-      <div className={shopProduct.shop}>
-        <div className={shopProduct.container}>
-          <div className={shopProduct.shopWrapper}>
-            <p className={shopProduct.shopTittle}>
+      <div className={shopProduct["shop"]}>
+        <div className={shopProduct["container"]}>
+          <div className={shopProduct["shopWrapper"]}>
+            <p className={shopProduct["shopTittle"]}>
               How you can help <br />
               our shelter
             </p>
-            <ul className={shopProduct.shopIcon - list}>
+            <ul className={shopProduct["shopIcon-list"]}>
               {shopIcon.map((icon) => (
-                <li className={shopProduct.shopIcon - item} key={icon.id}>
-                  <a className={shopProduct.shopIcon - link} href={icon.path}>
-                    {icon.title}
-                  </a>
+                <li className={shopProduct["shopIcon-item"]} key={icon.id}>
+                  <img
+                    className={shopProduct["shopIcon-link"]}
+                    src={icon.icon}
+                    href="#"
+                  ></img>
+                  <p className={shopProduct["shopIcon-text"]}> {icon.title} </p>
                 </li>
               ))}
             </ul>
