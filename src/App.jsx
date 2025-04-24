cb61ca469bbd72bf6e8b2c18a660d1051ad18f5b
Doni <ruslanovdoni05@gmail.com>
2025-04-24 13:49:06 +0600

    team

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
-import { Header } from "./components/Header/Header";
-import { About } from "./components/About/About";
-import { Slaider } from "./components/Slaider/Slaider";
-import { Shop } from "./components/Shoop/Shop";
-import { Aboutdog } from "./components/Aboutdog/Aboutdog";
-import { Footer } from "./components/Foooter/Footer";
+// import { Header } from "./components/Header/Header";
+// import { About } from "./components/About/About";
+// import { Slaider } from "./components/Slaider/Slaider";
+// import { Shop } from "./components/Shoop/Shop";
+// import { Aboutdog } from "./components/Aboutdog/Aboutdog";
+// import { Footer } from "./components/Foooter/Footer";
+import MyRoutes from "../../routes/MyRoutes";
 
 function App() {
   return (
     <>
-      <Header />
+      <MyRoutes />
+      {/* <Header />
       <About />
       <Slaider />
       <Shop />
       <Aboutdog />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
