ca5470d2f9d8e8610915091685a394e312834037
Aisulu <aisulubaturbekova@gmail.com>
2025-04-26 08:57:30 +0600

    footer

--- src/components/Foooter/Footer.jsx
+++ src/components/Foooter/Footer.jsx
@@ -4,7 +4,31 @@ import fo from './Footer.module.css'
 export const Footer = () => {
   return (
     <>
-      <h1 className={fo.text}>Footer</h1>
+    <div className={fo.contactSection}>
+      <div className={fo.contactBlock}>
+        <h2>
+          For questions <br /> and suggestions
+        </h2>
+        <div className={fo.contactInfo}>📧 email@shelter.com</div>
+        <div className={fo.contactInfo}>📞 +13 674 567 75 54</div>
+      </div>
+
+      <div className={fo.contactBlock}>
+        <h2>
+          We are waiting <br /> for your visit
+        </h2>
+        <div className={fo.contactInfo}>
+          📍 1 Central Street, Boston <br />
+          (entrance from the store)~
+        </div>
+        <div className={fo.contactInfo}>📍 18 South Park, London</div>
+      </div>
+
+      <div className={fo.contactBlock}>
+        <img src={dogImage} alt="Cute Dog" className={fo.dogImage} />
+      </div>
+    </div>
+
     </>
   )
 }
